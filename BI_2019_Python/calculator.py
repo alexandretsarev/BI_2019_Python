964150278e69ff3c75d11da494c1ba083a9b9dd3
alexandretsarev <alexandretsarev@gmail.com>
2019-09-25 10:28:42 +0300

    adding updated calculator and .travis.yml file

--- BI_2019_Python/calculator.py
+++ BI_2019_Python/calculator.py
@@ -1,28 +1,44 @@
-print("if you want to stop the calculator - x or y or command must be 'stop' but not empty ")
-print("commands can be + or - or / or * or **")
+print("\n\n")
+print("This calculator works with 3 inputs: the first value, the command and the second value")
+print("Yhe commands can be '+' or '-' or '/' or '*' or '**' or 'stop'")
+print("If you want to stop the calculator, the command must be 'stop' ")
+print("If the values are empty or aren't numeric, the calculator will be stopped as well")
+print("\n\n")
+
+commands = ["+", "-", "/", "*", "**", "stop"]  # all possible commands you can use for your calculations
 
 while True:
-
-    x = input("The 1st value is ")
-    s = input("The command is ")
-    y = input("The 2nd value is ")
-
-    if s == "stop" or x == "stop" or y == "stop":
-        print("The end")
+    # Now it checks whether the input (the first and the second value are numeric)
+    # If not -> the calculator will be closed
+    try:
+        x = int(input("Enter the first number: "))
+        s = input("Enter the command: ")
+        y = int(input("Enter the second number: "))
+    except ValueError:
+        print("Error! The 1st or the 2nd value is not numeric or is empty. The calculator is stopped")
         break
-    elif s == "+":
-        print(int(x) + int(y))
-    elif s == "-":
-        print(int(x) - int(y))
-    elif s == "*":
-        print(int(x) * int(y))
-    elif s == "/":
-        print(int(x) / int(y))
-    elif s == "**":
-        print(int(x) ** int(y))
+
+    if s in commands:  # now it checks whether the calculator knows an inputted command
+        if s == "+":
+            print(x + y, "\n\n")
+        elif s == "-":
+            print(x - y, "\n\n")
+        elif s == "*":
+            print(x * y, "\n\n")
+        elif s == "/":
+            try:  # check whether a user wants to divide by zero, if yes -> the programme will be closed
+                print(x / y, "\n\n")
+            except ZeroDivisionError:
+                print("Error! You cannot divide by zero!\n\n")
+        elif s == "**":
+            print(x ** y, "\n\n")
+
+        elif s == "stop":  # stop-word to stop the calculations, namely the command must be "stop"
+            print("The end of the calculations")
+            break
 
     else:
-        print("your input is wrong, try it again")
-
-
+        print("Error! There is no command like this or the command is empty")
+        print("Try it again or make the command 'stop' in the next iteration to stop the calculator")
+        print("\n\n")
 
