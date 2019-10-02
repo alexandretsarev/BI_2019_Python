8cee9527db88795c133cb9171209323f37d3528b
alexandretsarev <alexandretsarev@gmail.com>
2019-10-02 21:16:39 +0300

    add remove_unique_el.py file

---
+++ remove_unique_el.py
@@ -0,0 +1,14 @@
+# this function removes unique values from given list returning the list with
+# the repeated numbers
+
+
+def remove_unique_el(your_list):
+    list_wo_unique = []
+    for element in your_list:
+        if your_list.count(element) > 1:
+            list_wo_unique.append(element)
+        else:
+            pass
+    return list_wo_unique
+
+
