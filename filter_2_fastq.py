7280670fd6396a717ff5b84c53b0b3a15dc08bd8
alexandretsarev <alexandretsarev@gmail.com>
2020-04-22 03:03:07 +0300

    first comint trimmer

--- filter_2_fastq.py
+++ filter_2_fastq.py
@@ -1,39 +1,124 @@
 #!/usr/bin/env python
 
+from collections import namedtuple
 
-def len_read(seq):
-    return len(seq)
-
-
-def gc_content_count(seq):
-    return (seq.count("G") + seq.count("C")) / len(seq) * 100 if len(seq) != 0 else 0
+# read and read report structures
+Read = namedtuple('read', ['name', 'sequence', 'description', 'quality'])
+Read_report = namedtuple('read_report', ['read_valid', 'length_valid', 'gc_content_valid'])
 
 
-def read_approval(seq, min_len, gc_min, gc_max):
-    return True if len_read(seq) >= min_len and gc_min <= gc_content_count(seq) <= gc_max else False
+def length_read(input_read):
+    return len(input_read.sequence)
+
+
+def gc_content_count(input_read):
+    return (input_read.sequence.count("G") + input_read.sequence.count("C")) / len(input_read.sequence) * 100 \
+        if len(input_read.sequence) != 0 else 0
+
+
+def crop(inputread, threshold):
+    return Read(inputread.name, inputread.sequence[:threshold], inputread.description, inputread.quality[:threshold])
+
+
+def headcrop(input_read, threshold):
+    return Read(input_read.name, input_read.sequence[threshold:], input_read.description,
+                input_read.quality[threshold:])
+
+
+def quality_decipher(input_read):
+    return list(map(lambda ascii_symbol: ord(ascii_symbol) - 33, input_read.quality))
+
+
+def trailing(input_read, threshold):
+    read_quality = quality_decipher(input_read)
+    cut_position = next((idx for idx, nucl_quality in enumerate(read_quality) if nucl_quality < threshold), None)
+    return Read(input_read.name,
+                input_read.sequence[:cut_position],
+                input_read.description,
+                input_read.quality[:cut_position])
+
+
+def leading(input_read, threshold):
+    read_quality = quality_decipher(input_read)
+    cut_position = next((idx for idx, nucl_quality in reversed(list(enumerate(read_quality)))
+                         if nucl_quality < threshold), None)
+    return Read(input_read.name,
+                input_read.sequence[cut_position:],
+                input_read.description,
+                input_read.quality[cut_position:])
+
+
+def sliding_window(input_read, threshold, window):
+    read_quality = quality_decipher(input_read)
+    cut_position = next(
+        (idx for idx in range(len(read_quality) - window + 1) if
+         sum(read_quality[idx:idx + window]) / window < threshold), None)
+    return Read(input_read.name,
+                input_read.sequence[:cut_position],
+                input_read.description,
+                input_read.quality[:cut_position])
+
+
+def read_approval_report(input_read, min_length, gc_min, gc_max):
+    length_not_valid = 0 if length_read(input_read) >= min_length else 1
+    gc_content_not_valid = 0 if gc_min <= gc_content_count(input_read) <= gc_max else 1
+    read_valid = True if length_not_valid == 0 and gc_content_not_valid == 0 else False
+    return Read_report(read_valid, length_not_valid, gc_content_not_valid)
+
+
+def update_statistics_per_read(statistics_dict, one_read_report):
+    statistics_dict['n_total'] += 1
+    statistics_dict['n_failed_by_length'] += one_read_report.length_valid
+    statistics_dict['n_failed_by_gc_content'] += one_read_report.gc_content_valid
+    statistics_dict['n_valid'] += 1 if one_read_report.read_valid else 0
+    statistics_dict['n_failed'] += 1 if not one_read_report.read_valid else 0
+    return statistics_dict
+
+
+def generate_statistics_summary(statistics_dict):
+    proportion_valid = round(statistics_dict["n_valid"] / statistics_dict["n_total"] * 100, 3)
+    proportion_failed = round(statistics_dict["n_failed"] / statistics_dict["n_total"] * 100, 3)
+    failed_by_length_part = round(
+        statistics_dict["n_failed_by_length"] / statistics_dict["n_total"] * 100, 3)
+    failed_by_gc_part = round(
+        statistics_dict["n_failed_by_gc_content"] / statistics_dict["n_total"] * 100, 3)
+
+    return "\n".join(["TRIMMER STATISTICS:", f"Total number of reads {statistics_dict['n_total']}",
+                      f"Total valid reads {statistics_dict['n_valid']} ({proportion_valid}%)",
+                      f"Total failed reads {statistics_dict['n_failed']} ({proportion_failed}%)",
+                      f"Failed by length reads {statistics_dict['n_failed_by_length']} ({failed_by_length_part}%)",
+                      f"Failed by GC-content reads {statistics_dict['n_failed_by_gc_content']} ({failed_by_gc_part}%)"])
 
 
 if __name__ == '__main__':
     import argparse
     from os import path, remove
 
-    parser = argparse.ArgumentParser(description="This programm can work with .fastq files, it can:\n"
-                                                 "1) trimm reads by length\n"
-                                                 "2) trimm reads by GC content (in %)\n"
-                                                 "3) keep filtered and non-filtered reads")
+    parser = argparse.ArgumentParser(description="programm description")
     parser.add_argument('input', help="input fastq file")
     parser.add_argument('-ml', '--min_length', type=int, required=False, default=0,
                         help='filter by minimal length of the read')
     parser.add_argument('-gc', '--gc_bounds', nargs='+', required=False, type=int,
-                        help='percent range of GC content (default: 0,100)', metavar='')
+                        help='percent range of GC content', metavar='')
     parser.add_argument('-o', '--output_basename', nargs=1, required=False, type=str, metavar='')
-    parser.add_argument('-kf', '--keep_filtered', action="store_true", help='flag which keeps filtered'
-                                                                            ' reads in separate file')
+    parser.add_argument('-kf', '--keep_filtered', action="store_true", help='keep filtered reads in a separate file')
+    parser.add_argument('-stat', '--stat_summary', action="store_true", help='keep summary statistics in file')
+    parser.add_argument('-c', '--CROP', nargs=1, required=False, type=int, metavar='',
+                        help='cut the read to a specified length by removing bases from the end')
+    parser.add_argument('-hc', '--HEADCROP', nargs=1, required=False, type=int, metavar='',
+                        help='cut the specified number of bases from the start of the read')
+    parser.add_argument('-t', '--TRAILING', nargs=1, required=False, type=int, metavar='',
+                        help='cut bases off the end of a read, if below a threshold quality')
+    parser.add_argument('-l', '--LEADING', nargs=1, required=False, type=int, metavar='',
+                        help='cut bases off the start of a read, if below a threshold quality')
+    parser.add_argument('-s', '--SLIDINGWINDOW', nargs=2, required=False, type=int, metavar='',
+                        help='performs a sliding window trimming approach. It starts scanning at the 5-prime end and '
+                             'clips the read once the average quality within the window falls below a threshold')
     args = parser.parse_args()
 
     assert args.min_length >= 0, "Filter by length can't be less than zero"
-    assert len(args.gc_bounds) in [1, 2], "GC% range has two restrictions: more and less than something" \
-                                          " (e.g. -gc 10 50)"
+    assert len(args.gc_bounds) <= 2, "GC% range has two restrictions: more and less than something" \
+                                     " (e.g. -gc 10 50)"
     if len(args.gc_bounds) == 2:
         assert 0 <= (args.gc_bounds[0] and args.gc_bounds[1]) <= 100, "GC% range must be within 100 %"
     else:
@@ -45,22 +130,56 @@ if __name__ == '__main__':
     if args.output_basename is None:
         valid_path = args.input.replace(".fastq", "") + "__passed.fastq"
         non_valid_path = args.input.replace(".fastq", "") + "__failed.fastq"
+        stat_summary_path = args.input.replace(".fastq", "") + "__statistics.txt"
     else:
         valid_path = args.output_basename[0] + "__passed.fastq"
         non_valid_path = args.output_basename[0] + "__failed.fastq"
+        stat_summary_path = args.output_basename[0] + "__statistics.txt"
 
     remove(non_valid_path) if path.exists(non_valid_path) else None
 
-    with open(args.input, 'r') as fastq_data, open(valid_path, 'w') as valid_fq:
-        for line in fastq_data:
-            seq_id = line.strip()
-            sequence = next(fastq_data).strip()
-            description = next(fastq_data).strip()
-            quality = next(fastq_data).strip()
+    assert args.SLIDINGWINDOW[1] >= 1, "window in SLIDINGWINDOW cannot be less that 1 nucleotide"
+    assert args.TRAILING[0] > 0, "sequence quality cannot be less than 1"
+    assert args.LEADING[0] > 0, "sequence quality cannot be less than 1"
 
-            if read_approval(sequence, args.min_length, gc_min, gc_max):
-                valid_fq.writelines([seq_id, '\n', sequence, '\n', description, '\n', quality, '\n'])
+    with open(args.input, 'r') as fastq_data, open(valid_path, 'w') as valid_fq:
+
+        # summary statistics of the whole fastq file
+        fastq_statistics = {"n_total": 0,
+                            "n_failed_by_length": 0,
+                            "n_failed_by_gc_content": 0,
+                            "n_failed": 0,
+                            "n_valid": 0}
+
+        for line in fastq_data:
+            read = Read(line.strip(), next(fastq_data).strip(), next(fastq_data).strip(), next(fastq_data).strip())
+
+            # trimmomatic block
+            if args.CROP:
+                read = crop(read, args.CROP[0])
+            if args.HEADCROP:
+                read = headcrop(read, args.HEADCROP[0])
+            if args.TRAILING:
+                read = trailing(read, args.TRAILING[0])
+            if args.LEADING:
+                read = leading(read, args.LEADING[0])
+            if args.SLIDINGWINDOW:
+                read = sliding_window(read, args.SLIDINGWINDOW[0], args.SLIDINGWINDOW[1])
+
+            # statistics update block
+            read_report = read_approval_report(read, args.min_length, gc_min, gc_max)
+            fastq_statistics = update_statistics_per_read(fastq_statistics, read_report)
+
+            if read_report.read_valid:
+                valid_fq.writelines([read.name, '\n', read.sequence, '\n', read.description, '\n', read.quality, '\n'])
             else:
                 if args.keep_filtered:
                     with open(non_valid_path, 'a') as non_valid_fq:
-                        non_valid_fq.writelines([seq_id, '\n', sequence, '\n', description, '\n', quality, '\n'])
+                        non_valid_fq.writelines(
+                            [read.name, '\n', read.sequence, '\n', read.description, '\n', read.quality, '\n'])
+        # statistics summary block
+        if args.stat_summary:
+            with open(stat_summary_path, 'w') as summary_statistics:
+                summary_statistics.writelines(generate_statistics_summary(fastq_statistics))
+
+        print(generate_statistics_summary(fastq_statistics))
--- tests/test_filter2_fastq.py
+++ tests/test_filter2_fastq.py
@@ -2,35 +2,73 @@
 
 import unittest
 
-from ..filter_2_fastq import len_read, gc_content_count, read_approval
+from ..filter_2_fastq import length_read, gc_content_count, quality_decipher, crop, headcrop, trailing, leading, \
+    sliding_window, read_approval_report, update_statistics_per_read, generate_statistics_summary
+from collections import namedtuple
+
+Read = namedtuple('read', ['name', 'sequence', 'description', 'quality'])
+Read_report = namedtuple('read_report', ['read_valid', 'length_valid', 'gc_content_valid'])
+
+test_read_1 = Read("@SRR521461.17063732",
+                   "AGAGCATTTTAAGAGGTGCAGCCTCTGGAAGTGGATCAAACTAGAACTCATATGCCATACTAGATATGTTTGTCAA",
+                   "+", "GGGGGGGGGFGGGGGGGFGGEGGGGGGGGGFFGGGGGFGGGGGGFGGFGGGGFFEEGGGBDDDBEEFEEEDEE=ED")
+
+test_read_2 = Read("@SRR521461.17063732", "", "+", "")
+test_read_3 = Read("@SRR521461.17063732", "GGGGAAAATTTTCCCC", "+", "GGGGGGGGGGGG")
 
 
 class FilterTest(unittest.TestCase):
 
     def test_len_sequence(self):
-        self.assertEqual(len_read("GCAT"), 4)
+        self.assertEqual(length_read(test_read_1), 76)
 
-    def test_len_sequence_zero_length(self):
-        self.assertEqual(len_read(""), 0)
+    def test_len_sequence_zero(self):
+        self.assertEqual(length_read(test_read_2), 0)
 
     def test_gc_content_count(self):
-        self.assertEqual(gc_content_count("GCAT"), 50)
+        self.assertEqual(gc_content_count(test_read_3), 50)
 
-    def test_gc_content_count_zero_GC(self):
-        self.assertEqual(gc_content_count("ATATATATA"), 0)
+    def test_gc_content_count_zero(self):
+        self.assertEqual(gc_content_count(test_read_2), 0)
 
-    def test_gc_content_read_approval_no_thresholds(self):
-        self.assertEqual(read_approval("AGATA", 0, 0, 100), True)
+    def test_gc_quality_decipher(self):
+        self.assertEqual(quality_decipher(test_read_3), [38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38])
 
-    def test_gc_content_read_approval_length_filter(self):
-        self.assertEqual(read_approval("ATATGCGC", 20, 1, 100), False)
+    def test_crop_sequence(self):
+        self.assertEqual(crop(test_read_1, 35).sequence, "AGAGCATTTTAAGAGGTGCAGCCTCTGGAAGTGGA")
 
-    def test_gc_content_read_approval_gc_filter(self):
-        self.assertEqual(read_approval("ATATGCGC", 0, 70, 100), False)
+    def test_crop_sequence_2(self):
+        self.assertEqual(crop(test_read_1, 1).sequence, "A")
 
-    def test_gc_content_read_approval_length_gc_filter(self):
-        self.assertEqual(read_approval("ATATGCGC", 50, 70, 100), False)
+    def test_crop_quality(self):
+        self.assertEqual(crop(test_read_1, 35).quality, "GGGGGGGGGFGGGGGGGFGGEGGGGGGGGGFFGGG")
 
+    def test_crop_quality_2(self):
+        self.assertEqual(crop(test_read_1, 1).quality, "G")
+
+    def test_headcrop_sequence(self):
+        self.assertEqual(headcrop(test_read_1, 35).sequence, "TCAAACTAGAACTCATATGCCATACTAGATATGTTTGTCAA")
+
+    def test_headcrop_quality(self):
+        self.assertEqual(headcrop(test_read_1, 35).quality, "GGFGGGGGGFGGFGGGGFFEEGGGBDDDBEEFEEEDEE=ED")
+
+    def test_trailing_sequence(self):
+        self.assertEqual(trailing(test_read_1, 37).sequence, "AGAGCATTTTAAGAGGTGCA")
+
+    def test_trailing_quality(self):
+        self.assertEqual(trailing(test_read_1, 37).quality, "GGGGGGGGGFGGGGGGGFGG")
+
+    def test_leading_sequence(self):
+        self.assertEqual(leading(test_read_1, 35).sequence, "CAA")
+
+    def test_leading_quality(self):
+        self.assertEqual(leading(test_read_1, 35).quality, "=ED")
+
+    def test_sliding_window_sequence(self):
+        self.assertEqual(sliding_window(test_read_1, 38, 5).sequence, "AGAGC")
+
+    def test_sliding_window_quality(self):
+        self.assertEqual(sliding_window(test_read_1, 38, 5).quality, "GGGGG")
 
 if __name__ == '__main__':
     unittest.main()
